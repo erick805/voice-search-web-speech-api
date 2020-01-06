07507a98b432414b09e1f890ef43e27651f7cf02
Ricky Lau <rickylaufitness@gmail.com>
2020-01-06 12:25:03 -0500

    add: speech recognition global

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const searchForm = document.querySelector("#search-form");
 const searchFormInput = searchForm.querySelector("input");
 const micBtn = searchForm.querySelector("button");
 const micIcon = micBtn.querySelector("i");
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if (SpeechRecognition) {
+  console.log("Your Browser supports speech recognition");
+} else {
+  console.log("Your Browser does not support speech recognition");
+}
