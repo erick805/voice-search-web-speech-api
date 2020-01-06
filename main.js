9bbb1483016a18a43a31f82c87ec7ec49d1ccb43
Ricky Lau <rickylaufitness@gmail.com>
2020-01-06 12:29:56 -0500

    add: browser speech recognition dynamic microphone view

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const searchForm = document.querySelector("#search-form");
 const searchFormInput = searchForm.querySelector("input");
-const micBtn = searchForm.querySelector("button");
-const micIcon = micBtn.querySelector("i");
 
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
 if (SpeechRecognition) {
   console.log("Your Browser supports speech recognition");
+
+  searchForm.insertAdjacentHTML(
+    "beforeend",
+    '<button type="button"><i class="fa fa-microphone"></i></button>'
+  );
+
+  const micBtn = searchForm.querySelector("button");
+  const micIcon = micBtn.querySelector("i");
 } else {
   console.log("Your Browser does not support speech recognition");
 }
