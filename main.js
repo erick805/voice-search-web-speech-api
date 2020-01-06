fe70adf7bbb48949296ef61379fe4ec909928361
Ricky Lau <rickylaufitness@gmail.com>
2020-01-06 12:20:30 -0500

    add: dom constants in main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const searchForm = document.querySelector("#search-form");
+const searchFormInput = searchForm.querySelector("input");
+const micBtn = searchForm.querySelector("button");
+const micIcon = micBtn.querySelector("i");
